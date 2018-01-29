e1397a09bc4ab0e1116ddb450d3c06d4205114be
Vincent Geng <vincentgeng90@gmail.com>
2018-01-29 23:00:27 +0800

    #First Commit

---
+++ src/main/java/com/vincent/lab/RabbitMqLoadTestApplication.java
@@ -0,0 +1,12 @@
+package com.vincent.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqLoadTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitMqLoadTestApplication.class, args);
+	}
+}
